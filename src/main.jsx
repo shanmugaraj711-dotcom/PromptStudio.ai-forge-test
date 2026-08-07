544df820f3ccc86e9a6361a48485e57101f7142b
shanmugaraj711-dotcom <shanmugaraj711@gmail.com>
2026-08-07 23:57:11 +0530

    Wrap App component with BrowserRouter and AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+
+import "./styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
