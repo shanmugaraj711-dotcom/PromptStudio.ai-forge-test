f8b407e8e825763c418c70019327fb35cff1b6ef
shanmugaraj711-dotcom <shanmugaraj711@gmail.com>
2026-09-04 00:02:17 +0530

    feat: configure Forge web origin

--- apk-forge/android/app/build.gradle.kts
+++ apk-forge/android/app/build.gradle.kts
@@ -13,6 +13,11 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
+        buildConfigField("String", "FORGE_WEB_ORIGIN", "\"https://promptstudioai.in\"")
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 
     buildTypes {
